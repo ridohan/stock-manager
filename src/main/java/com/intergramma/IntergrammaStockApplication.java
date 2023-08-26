30114bc3bbffb5d015de8fcc0b24c35ada89e048
Rédouane Khiter <red.khiter.pro@gmail.com>
2023-08-26 12:42:43 +0200

    Initial commit

---
+++ src/main/java/com/intergramma/IntergrammaStockApplication.java
@@ -0,0 +1,15 @@
+package com.intergramma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergrammaStockApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(IntergrammaStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intergramma/stock/intergrammastock/IntergrammaStockApplicationTests.java
@@ -0,0 +1,15 @@
+package com.intergramma.stock.intergrammastock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntergrammaStockApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
