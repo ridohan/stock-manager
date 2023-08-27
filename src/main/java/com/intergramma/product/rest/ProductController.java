341e1c5cd9a9a2b3cb627a19c5226aaf20e091b0
Rédouane Khiter <red.khiter.pro@gmail.com>
2023-08-27 17:39:58 +0200

    FEAT : Product Rest Layer

--- src/main/java/com/intergramma/product/rest/ProductController.java
+++ src/main/java/com/intergramma/product/rest/ProductController.java
@@ -2,6 +2,11 @@ package com.intergramma.product.rest;
 
 import com.intergramma.product.Product;
 import com.intergramma.product.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +23,11 @@ public class ProductController
 		this.productService = productService;
 	}
 
+	@Operation(summary = "Get All products")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200",
+			description = "Retrieved all products",
+			content = { @Content(mediaType = "application/json",
+					schema = @Schema(implementation = Product.class)) }) })
 	@GetMapping("/products")
 	List<Product> getAllProducts()
 	{
